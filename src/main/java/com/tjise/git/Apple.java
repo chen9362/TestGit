d78f29664d31c032b16e7103e6116c14295ff054
eclipse_user <eclipse_user@aliyun.com>
2020-06-13 20:38:28 +0800

    test hot_fix

--- src/main/java/com/tjise/git/Apple.java
+++ src/main/java/com/tjise/git/Apple.java
@@ -3,6 +3,8 @@ package com.tjise.git;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("test......left");
+		
+		System.out.println("test ...hot_fix ....");
 
 	}
 
